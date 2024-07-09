2a54e867e1c870e4b00a36efe56fad40b3e870b3
test-learn-ajr-tsd <dim.sergeev2000@vk.com>
2024-07-09 09:52:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Radzen;
+using ЗаданиеТестСтажировкаМагнитПриложениеBlazor.Components;
+
+namespace ЗаданиеТестСтажировкаМагнитПриложениеBlazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<ContextMenuService>();
+            builder.Services.AddScoped<DialogService>();
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
---
+++ Данные/Контрагент.cs
@@ -0,0 +1,9 @@
+﻿namespace ЗаданиеТестСтажировкаМагнитПриложениеBlazor.Данные
+{
+    public class Контрагент
+    {
+        public int Id { get; set; }
+        public string Наименование { get; set; }
+        public string? Адрес {  get; set; }
+    }
+}
